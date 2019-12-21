aeebf0c939bd73b73957de608d7c1f49e4331f6f
XipingHu <hxp201406@gmail.com>
2019-12-21 21:03:18 +0800

    Added auto-complete and spell checking

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,5 @@
-(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
-(package-initialize) ;; You might already have this line
+(package-initialize)
+
 (add-hook 'LaTeX-mode-hook 'turn-on-cdlatex)
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
@@ -9,7 +8,31 @@
 ;; correlate
 (setq TeX-source-correlate-mode t)
 
-(custom-set-variables '(TeX-source-correlate-method (quote synctex)) '(TeX-source-correlate-mode t) '(TeX-source-correlate-start-server t) '(TeX-view-program-list (quote (("Sumatra PDF" "\"C:/Program Files (x86)/SumatraPDF/SumatraPDF.exe\" -reuse-instance %o")))))(custom-set-faces)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-source-correlate-method (quote synctex))
+ '(TeX-source-correlate-mode t)
+ '(TeX-source-correlate-start-server t)
+ '(TeX-view-program-list
+   (quote
+    (("Sumatra PDF" "\"C:/Program Files (x86)/SumatraPDF/SumatraPDF.exe\" -reuse-instance %o"))))
+ '(package-archives
+   (quote
+    (("GNU ELPA" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+     ("MELPA" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+     ("MELPA Stable" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/")
+     ("Marmalade" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/marmalade/")
+     ("Org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/"))))
+ '(package-selected-packages (quote (auto-complete yasnippet-snippets cdlatex auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 (setq TeX-view-program-selection '((output-pdf "SumatraPDF")))
 
 ;; Update PDF buffers after successful LaTeX runs
@@ -22,23 +45,19 @@
 ;; Enable yasnippet
 (yas-global-mode t)
 
+;; Enable auto-complete
+(add-hook 'text-mode-hook 'auto-complete-mode)
+(add-hook 'prog-mode-hook 'auto-complete-mode)
+
+;; Spell Check
+(setq-default ispell-program-name "C:/msys64/mingw64/bin/hunspell.exe")
+(setq ispell-local-dictionary "en_US") 
+(setq ispell-local-dictionary-alist
+      '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8)))
+(add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+
+
 (setq package-check-signature nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-archives
-   (quote
-    (("GNU ELPA" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-     ("MELPA" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-     ("MELPA Stable" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/")
-     ("Marmalade" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/marmalade/")
-     ("Org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/"))))
- '(package-selected-packages (quote (yasnippet-snippets cdlatex auctex))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+
